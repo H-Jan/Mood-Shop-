c7b99bf29981ba54cd4edd23535cd3c70585b75e
Hani Jandali <hanijandali@hanis-mbp.lan>
2020-09-29 16:38:41 -0700

    add button connected to cart and moved Javascript to script.js file

--- resources/js/script.js
+++ resources/js/script.js
@@ -44,3 +44,112 @@ for (let i = 0; i < data.length; ++i) {
     itemsContainer.appendChild(newDiv)
 
 }
+
+const itemList = document.getElementById('item-list')
+console.log(itemList)
+
+const cartQty = document.getElementById('cart-qty')
+console.log(cartQty)
+
+const cartTotal = document.getElementById('cart-total')
+console.log(cartTotal)
+
+
+itemList.innerHTML = '<li> Hello World </li>'
+
+const cart = []
+// -----------------------------------------
+// ADD ITEMS
+function addItems(name, price) {
+    for (let i = 0; i < cart.length; i += 1) {
+        if (cart[i].name === name) {
+            cart[i].qty += 1
+            // So this for loop needs to keep running, but needs to stop for below function
+            return
+            //return ends the for loop and function itself, stopping the below code for const.push(item)
+        }
+    }
+    const item = { name, price, qty: 1 }
+    cart.push(item)
+
+
+}
+// -------------------------------------------
+// SHOW ITEMS 
+function showItems() {
+
+    const qty = gettingQty()
+    cartQty.innerHTML = `You have ${gettingQty()} items in your cart`
+
+    let itemStr = ' '
+    for (let i = 0; i < cart.length; i += 1) {
+        // console.log(`${cart[i].name} $${cart[i].price} x ${cart[i].qty}`)
+        // { name: 'Apple', price: 0.99, qty: 3}
+        const { name, price, qty } = cart[i]
+
+        itemStr += `<li> ${name} $${price} x ${qty} = ${qty * price} </li>`
+
+    }
+    itemList.innerHTML = itemStr
+
+    cartTotal.innerHTML = `Total in cart: $${gettingTotal()}`
+
+
+
+
+}
+const all_items_button = Array.from(document.querySelectorAll('button'))
+console.log(all_items_button)
+all_items_button.forEach(elt => elt.addEventListener('click', () => {
+    addItems(elt.getAttribute('id'), elt.getAttribute('data-price'))
+    showItems()
+}))
+
+
+function gettingQty() {
+    //Did not know how to demonstrate quantity on own
+    let qty = 0
+    for (let i = 0; i < cart.length; i += 1) {
+        qty += cart[i].qty
+    }
+    return qty
+}
+// scope is a term to define where a value is visible.
+// Quantity is only visible in the above function block
+// the return qty allows the function to run above by calling the function
+
+//----------------------------------
+// gettingTotal 
+function gettingTotal() {
+    let totalPrice = 0
+    for (let i = 0; i < cart.length; i += 1) {
+        totalPrice += cart[i].price * cart[i].qty
+        // As it is going down the cart.length, it will add the price times the quantity to the total price
+        //until it reaches the bottom
+    }
+
+    return totalPrice.toFixed(3)
+    // toFixed rounds off the number of decimal places 
+    // toFixed returns a string, so should be last step as a 'display' for math
+
+}
+//-------------------------------------
+// REMOVE ITEM 
+function removeItems(name, qty = 0) {
+    for (let i = 0; i < cart.length; i += 1) {
+        if (cart[i].name === name) {
+            if (qty > 0) {
+                cart[i].qty -= qty
+            }
+
+            if (cart[i].qty < 1 || qty === 0) {
+                cart.splice(i, 1)
+            }
+
+            return
+        }
+    }
+
+
+}
+
